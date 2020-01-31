023902a535bbd82b1810a8d9c5bae804c70a1c4d
Jonathan Andrade <jearsoft@gmail.com>
2020-01-31 13:52:31 -0600

    feature: Adding AutoMapper and  Logger

---
+++ Core/Profiles/Profiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Repository.Models;
+using Repository.Models.Dtos;
+
+namespace Core.Profiles
+{
+    public class Profiles : Profile
+    {
+        public Profiles()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap();
+        }
+    }
+}

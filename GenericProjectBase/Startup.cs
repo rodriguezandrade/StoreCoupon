0f26b99e9f40596b3f167b113f100279edc29625
Jonathan Andrade <jearsoft@gmail.com>
2020-02-07 22:39:47 -0600

    feat: join angular with net core configuration

--- GenericProjectBase/Startup.cs
+++ GenericProjectBase/Startup.cs
@@ -2,6 +2,7 @@ using GenericProjectBase.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,8 +14,7 @@ namespace GenericProjectBase
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
-            //LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+        { 
             Configuration = configuration;
         }
 
@@ -24,6 +24,10 @@ namespace GenericProjectBase
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSpaStaticFiles(configuration =>
+            //{
+            //    configuration.RootPath = "App/dist";
+            //});
 
             services.ConfigureCors();
             services.ConfigureIISIntegration();
@@ -58,9 +62,6 @@ namespace GenericProjectBase
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseCors("CorsPolicy");
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
@@ -73,6 +74,15 @@ namespace GenericProjectBase
             {
                 endpoints.MapControllers();
             });
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "App";
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+
         }
     }
 }
